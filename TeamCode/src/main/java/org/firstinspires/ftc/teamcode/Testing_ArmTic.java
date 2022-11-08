4a4a2bd0e5a44cc524049c19a4dfb33de25ce7c6
IsaiahMcChen <ichen@newtech.coppellisd.com>
2022-11-08 11:31:17 -0600

    First edition of the Tic Measurer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing_ArmTic.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing_ArmTic.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2021 FIRST. All rights reserved.
+/* Copyright (c) 2017 FIRST. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted (subject to the limitations in the disclaimer below) provided that
@@ -29,165 +29,225 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 /**
- * This file contains an example of a Linear "OpMode".
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
  * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
  * The names of OpModes appear on the menu of the FTC Driver Station.
- * When a selection is made from the menu, the corresponding OpMode is executed.
+ * When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
  *
- * This particular OpMode illustrates driving a 4-motor Omni-Directional (or Holonomic) robot.
- * This code will work with either a Mecanum-Drive or an X-Drive train.
- * Both of these drives are illustrated at https://gm0.org/en/latest/docs/robot-design/drivetrains/holonomic.html
- * Note that a Mecanum drive must display an X roller-pattern when viewed from above.
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all iterative OpModes contain.
  *
- * Holonomic drives provide the ability for the robot to move in three axes (directions) simultaneously.
- * Each motion axis is controlled by one Joystick axis.
- *
- * 1) Axial:    Driving forward and backwards               Left-joystick Forward/Backwards
- * 2) Lateral:  Strafing right and left                     Left-joystick Right and Left
- * 3) Yaw:      Rotating Clockwise and counter clockwise    Right-joystick Right and Left
- *
- * This code is written assuming that the right-side motors need to be reversed for the robot to drive forward.
- * When you first test your robot, if it moves backwards when you push the left stick forward, then you must flip
- * the direction of all 4 motors (see code below).
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove ~or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Arm Tic Testing", group="Hunter")
-//@Disabled
-public class Testing_ArmTic extends LinearOpMode {
-
-    // Declare OpMode members for each of the 4 motors.
-    private ElapsedTime runtime = new ElapsedTime();
+@TeleOp(name="Tic Tac", group="Isaiah")
+//@Disabled  This way it will run on the robot
+public class Testing_ArmTic extends OpMode {
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();  //timer
 
     /*
-    Isaiah Paste This with the other instane variables
+    Declare motors to type DcMotorEx
+    Documentation:
+    https://ftctechnh.github.io/ftc_app/doc/javadoc/com/qualcomm/robotcore/hardware/DcMotorEx.html
      */
+    private DcMotorEx lazyS;
+    private DcMotorEx intake;
+    private DcMotorEx arm;
+    private Servo bucket;
+
+
     private DcMotorEx motorArm;
     private double maxTicsPerSec = 3000;
     private int targetPosition;
-    private DigitalChannel armMagnet;
-    private int armLevelFloor;
-    private int armLevelRide;
-    private int armLevelCanTilt;
-    private int armLevel2nd;
-    private int armLevel3rd;
-    private boolean armZeroOverride;
     private String buttonPressed;
 
+    static final double INCREMENT = 0.01;     // amount to slew servo each CYCLE_MS cycle
+    static final double MIN_POS = 0.0;     // Maximum rotational position
+    static final double MAX_POS = 1.0;     // Minimum rotational position\
+    private double curTicPos = 0;
+    private double ticMultiplier = 1;
+
+    double position = 0.0; // Start at bottom position
+    boolean rampUp = true;
+
+    private int level = 1; //1 = ground, 2 = level 2, 3 = level 3
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
     @Override
-    public void runOpMode() {
+    public void init() {
+        telemetry.addData("Status", "Initialized");
 
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        lazyS = hardwareMap.get(DcMotorEx.class, "lazyS");
+        intake = hardwareMap.get(DcMotorEx.class, "intake");
+        arm = hardwareMap.get(DcMotorEx.class, "arm");
+        bucket = hardwareMap.get(Servo.class,"bucket");
 
-         /*
-        Isaiah Paste this in init
-         */
         motorArm = hardwareMap.get(DcMotorEx.class, "arm");
         motorArm.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         motorArm.setDirection(DcMotorSimple.Direction.FORWARD);
         motorArm.setVelocityPIDFCoefficients(1.17, 0.117, 0, 11.7);
-        armMagnet = hardwareMap.get(DigitalChannel.class, "magneticSlide");
-        armMagnet.setMode(DigitalChannel.Mode.INPUT);
-        this.armZeroOverride = false;
-        this.armLevelFloor = 10;
-        this.armLevelRide = 500;
-        this.armLevelCanTilt = 1340;
-        this.armLevel2nd = 1015;
-        this.armLevel3rd = 1650;
         this.targetPosition = 0;
         this.buttonPressed = "";
 
 
-        // Wait for the game to start (driver presses PLAY)
+
+       /*
+           Set up motors so they run without the encoders
+           This way they run freely.  They won't go to a specific position or count the number of rotations
+           It will now run with range from -1.0 to 1.0
+           See Documentation for other encoder modes
+           https://docs.revrobotics.com/rev-control-system/programming/using-encoder-feedback
+        */
+        lazyS.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        intake.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        arm.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        intake.setDirection(DcMotorSimple.Direction.REVERSE);
+        arm.setDirection(DcMotorSimple.Direction.FORWARD);
+        position = 1.0;
+
+
+        // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
-        telemetry.update();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+        //mecanum wheel code
+        //https://ftccats.github.io/software/ProgrammingMecanumWheels.html
+
+        double lazyPower = 0.0;
+        double spinnerPower = 0.0;
+        double armPower = 0.0;
 
 
-        waitForStart();
-        runtime.reset();
+        //Go
+        if (gamepad1.right_bumper) { //Moves set motor forward 1 tic times how fast you chose
+            maxTicsPerSec = (1 * ticMultiplier);
+            curTicPos += maxTicsPerSec;
+            telemetry.addData("Current position: ", "%.1f tics", curTicPos);
+            telemetry.addData("Tic Multiplier", "%.1f", ticMultiplier);
+            telemetry.update();
+        }
+        else if (gamepad1.left_bumper){ //Moves set motor back 1 tic times how fast you chose
+            maxTicsPerSec = (-1 * ticMultiplier);
+            curTicPos += maxTicsPerSec;
+            telemetry.addData("Current position: ", "%.1f tics", curTicPos);
+            telemetry.addData("Tic Multiplier", "%.1f", ticMultiplier);
+            telemetry.update();
+        }
+
+        if (gamepad1.dpad_right){ //multiplies speed by 5 times
+            ticMultiplier *= 5;
+        }
+        else if (gamepad1.dpad_left) { //slows speed by 5 times
+            ticMultiplier /= 5;
+        }
+
+        telemetry.addData("position of servo", "%.1f", position);
 
 
 
+        //change the power for each wheel
 
 
 
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
+        lazyS.setPower(lazyPower);
+        intake.setPower(spinnerPower);
+        arm.setPower(armPower);
+        bucket.setPosition(position);
 
-            /*
-            Isaiah paste this in the main loop
-             */
-            //get input
-            if (gamepad2.x){ //2nd level
-                targetPosition = armLevel2nd;
-                buttonPressed = "X";
-            }
-            else if (gamepad2.y){  //3rd level
-                targetPosition = armLevel3rd;
-                buttonPressed = "Y";
-            }
-            else if (gamepad2.a){  //on floor
-                targetPosition = armLevelFloor;
-                buttonPressed = "A";
-            }
-            else if (gamepad2.b || gamepad1.b){  //riding
-                targetPosition = armLevelRide;
-                buttonPressed = "B";
-            }
-            else if(gamepad2.dpad_up) {
-                targetPosition += 10;
-                buttonPressed = "DPad_Up";
-            }
-            else if (gamepad2.dpad_down) {
-                targetPosition -= 10;
-                buttonPressed = "Dpad_Down";
-            }
-            else {
-                buttonPressed = "";
-            }
+
+        /*
+
+        PLAYER 2 GAMEPAD
+
+
+         */
+        //get input
+
+
+//        if
+
 
 
             /*
                 Keep the target position >= 0
                 Unless dpad_down is pressed
              */
-            if (targetPosition < 0 && !gamepad2.dpad_down){
-                targetPosition = 0;
-            }
+        if (arm.getCurrentPosition() == 0 && gamepad2.dpad_down){
+            targetPosition -= 10;
+        }
 
             /*
                 check if magnet is touching sensor
                 True = NOT touching sensor
              */
-            if (!armMagnet.getState()){ //not sure why it's backwards
-                motorArm.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-            }
 
 
-            //set motor
-            motorArm.setTargetPosition(targetPosition);
-            motorArm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-            motorArm.setVelocity(maxTicsPerSec);
 
-            telemetry.addData("Arm Button: ", buttonPressed);
-            telemetry.addData("At Arm Magnet: ", !armMagnet.getState());
-            telemetry.addData("Can turn servo: ", motorArm.getCurrentPosition() >= armLevelCanTilt);
-            telemetry.addData("Arm targetPosition: ", targetPosition);
-            telemetry.addData("Arm currentPosition: ", motorArm.getCurrentPosition());
-            telemetry.update();
+        //set motor
+        //motorArm.setTargetPosition(targetPosition);
+        motorArm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        motorArm.setVelocity(maxTicsPerSec);
+        telemetry.update();
 
 
-        }
-    }}
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+
+    }
+
+    public boolean distance(int cm) //cm = how far away to check
+    {
+        return true;
+    }
+}
+
+
